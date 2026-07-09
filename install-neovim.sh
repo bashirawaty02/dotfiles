2b74a1d5ba082c91b96abc5f107a298859362fe3
bashirawaty02 <25037042+bashirawaty02@users.noreply.github.com>
2026-07-09 20:14:58 +1000

    Refactor Neovim installation script for zsh
    Updated the installation script to use zsh, added build directory management, and improved cloning and installation steps.

--- install-neovim.sh
+++ install-neovim.sh
@@ -1,5 +1,38 @@
-cd /tmp
-git clone git@github.com:neovim/neovim.git
-cd neovim
-make CMAKE_BUILD_TYPE=Release
-make CMAKE_INSTALL_PREFIX=$HOME/.neovim install
+#!/usr/bin/env zsh
+set -euo pipefail
+
+NEOVIM_DIR="$HOME/.neovim"
+BUILD_DIR="/tmp/neovim-src"
+
+echo ">>> Building Neovim into $NEOVIM_DIR"
+
+###############################################################################
+# Clone Neovim source
+###############################################################################
+if [[ -d "$BUILD_DIR" ]]; then
+    echo ">>> Removing old build directory"
+    rm -rf "$BUILD_DIR"
+fi
+
+echo ">>> Cloning Neovim repository"
+git clone --depth=1 https://github.com/neovim/neovim.git "$BUILD_DIR"
+
+###############################################################################
+# Build Neovim
+###############################################################################
+echo ">>> Building Neovim (Release mode)"
+(
+    cd "$BUILD_DIR"
+    make CMAKE_BUILD_TYPE=Release
+)
+
+###############################################################################
+# Install Neovim
+###############################################################################
+echo ">>> Installing Neovim into $NEOVIM_DIR"
+(
+    cd "$BUILD_DIR"
+    make CMAKE_INSTALL_PREFIX="$NEOVIM_DIR" install
+)
+
+echo ">>> Neovim installed successfully at: $NEOVIM_DIR/bin/nvim"
