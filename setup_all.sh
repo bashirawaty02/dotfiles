e97edac0467ff228c2c355720ca4e3db59d1dba8
bashirawaty02 <25037042+bashirawaty02@users.noreply.github.com>
2026-07-09 20:16:41 +1000

    Refactor setup_all.sh for improved backup and stow
    Refactor backup function and improve error handling. Update dotfile backup process and streamline stowing of programs.

--- setup_all.sh
+++ setup_all.sh
@@ -1,47 +1,60 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-# set -e          # Exit on error
-# set -o pipefail # Exit on pipe error
-# set -x          # Enable verbosity
+###############################################################################
+# Remove macOS junk
+###############################################################################
+find . -name ".DS_Store" -exec rm -f {} \;
 
-# Dont link DS_Store files
-find . -name ".DS_Store" -exec rm {} \;
-
+###############################################################################
+# Programs to stow
+###############################################################################
 PROGRAMS=(alias bash env git python scripts stow tmux vim zsh)
-# PROGRAMS=(alias aspell bash env git latex python scripts stow tmux vim zsh mac terminal)
-OLD_DOTFILES="dotfile_bk_$(date -u +"%Y%m%d%H%M%S")"
-mkdir $OLD_DOTFILES
 
-function backup_if_exists() {
-    if [ -f $1 ];
-    then
-      mv $1 $OLD_DOTFILES
-    fi
-    if [ -d $1 ];
-    then
-      mv $1 $OLD_DOTFILES
+###############################################################################
+# Backup directory
+###############################################################################
+OLD_DOTFILES="dotfile_bk_$(date -u +"%Y%m%d%H%M%S")"
+mkdir -p "$OLD_DOTFILES"
+
+backup_if_exists() {
+    local path="$1"
+    if [[ -e "$path" ]]; then
+        echo ">>> Backing up $path → $OLD_DOTFILES"
+        mv "$path" "$OLD_DOTFILES/"
     fi
 }
 
-# Clean common conflicts
-backup_if_exists ~/.bash_profile
-backup_if_exists ~/.bashrc
-backup_if_exists ~/.zshrc
-backup_if_exists ~/.gitconfig
-backup_if_exists ~/.tmux.conf
-backup_if_exists ~/.profile
+###############################################################################
+# Backup common dotfiles
+###############################################################################
+backup_if_exists "$HOME/.bash_profile"
+backup_if_exists "$HOME/.bashrc"
+backup_if_exists "$HOME/.zshrc"
+backup_if_exists "$HOME/.gitconfig"
+backup_if_exists "$HOME/.tmux.conf"
+backup_if_exists "$HOME/.profile"
 
+###############################################################################
+# Backup Prezto runcoms
+###############################################################################
+if [[ -d "$HOME/.zprezto/runcoms" ]]; then
+    for f in "$HOME"/.zprezto/runcoms/z*; do
+        [[ -e "$f" ]] && mv "$f" "$OLD_DOTFILES/"
+    done
+fi
 
-mkdir -p ~/.vim/undodir
+###############################################################################
+# Vim directories
+###############################################################################
+mkdir -p "$HOME/.vim/undodir"
 
-for f in ~/.zprezto/runcoms/z*
-do
-    mv "$f" $OLD_DOTFILES
+###############################################################################
+# Stow dotfiles
+###############################################################################
+for program in "${PROGRAMS[@]}"; do
+    echo ">>> Stowing $program"
+    stow -v --target="$HOME" "$program"
 done
 
-for program in ${PROGRAMS[@]}; do
-  stow -v --target=$HOME $program
-  echo "Configuring $program"
-done
-
-echo "Done!"
+echo ">>> Done!"
